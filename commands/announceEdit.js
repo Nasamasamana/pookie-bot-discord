198df816dfb0faed36c5545c9648c89c7f3d9545
Nasamasamana <minhajrahmannasa@gmail.com>
2025-09-27 23:41:39 +0600

    Update announceEdit.js

--- commands/announceEdit.js
+++ commands/announceEdit.js
@@ -1,16 +1,36 @@
-// commands/announceEdit.js
-const createCmd = require('./announceCreate');
+import { getDraft, saveDraft } from '../utils/drafts.js';
+import { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
 
-module.exports = {
-  name: 'announceEdit',
-  async execute({ message, client }) {
-    try {
-      // reuse create behavior to (re)open builder in current channel
-      await createCmd.execute({ message, client });
-      return message.reply('Reopened builder in this channel.');
-    } catch (err) {
-      console.error('announceEdit error', err);
-      return message.reply('Failed to reopen builder.');
-    }
+export default {
+  name: 'announceedit', // prefix: p!announceedit
+  description: 'Edit your current announcement draft',
+  run: async (client, message, args) => {
+    const draft = getDraft(message.author.id);
+    if (!draft) return message.reply('No draft found. Use p!announcecreate first.');
+
+    const modal = new ModalBuilder()
+      .setCustomId('announce_modal')
+      .setTitle('Edit Announcement')
+      .addComponents(
+        new ActionRowBuilder().addComponents(
+          new TextInputBuilder()
+            .setCustomId('title')
+            .setLabel('Title')
+            .setStyle(TextInputStyle.Short)
+            .setPlaceholder('Enter your title')
+            .setValue(draft.title)
+        ),
+        new ActionRowBuilder().addComponents(
+          new TextInputBuilder()
+            .setCustomId('description')
+            .setLabel('Description')
+            .setStyle(TextInputStyle.Paragraph)
+            .setPlaceholder('Enter the description')
+            .setValue(draft.description)
+        )
+        // You can add more inputs here: color, footer, image, thumbnail
+      );
+
+    await message.showModal(modal);
   }
 };
