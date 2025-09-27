fbfe419c76204268fe24fd46374d0d56f9b9b09d
Nasamasamana <minhajrahmannasa@gmail.com>
2025-09-27 22:05:27 +0600

    Update announce_edit.js

--- commands/announce_edit.js
+++ commands/announce_edit.js
@@ -1,1 +1,11 @@
+// commands/announce_edit.js
+const createCmd = require('./announce_create');
 
+module.exports = {
+  name: 'announce_edit',
+  async execute({ message, client }) {
+    // reuse the create command behavior to (re)open the builder in the current channel
+    await createCmd.execute({ message, client });
+    return message.reply('Reopened builder in this channel.');
+  }
+};
